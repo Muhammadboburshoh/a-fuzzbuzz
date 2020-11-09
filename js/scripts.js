4dfad778a15b5d5101f3d2cd856a24b503227ab2
Muhammadboburshoh <muhammadbobur600@gmail.com>
2020-11-09 14:23:41 +0500

    Finishid fuzzBuzz

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var fuzz = 3;
+var buzz = 5;
+var fuzzBuzz = 15;
+
+var elForm = document.querySelector(".form");
+var result = document.querySelector(".result")
+
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault()
+
+  var inputNumber= parseFloat(elForm.querySelector(".number-input").value.trim(),10);
+
+  if (inputNumber < 0) {
+    alert("Iltimos musbat son kiriting :)")
+  }
+
+  if (inputNumber % fuzzBuzz === 0) {
+    result.textContent = "FuzzBuzz";
+  }
+  else if(inputNumber % buzz === 0) {
+    result.textContent = "Buzz";
+  }
+  else if( inputNumber % fuzz === 0){
+    result.textContent = "Fuzz";
+  }
+  else {
+    result.textContent = inputNumber;
+  }
+})
+
+
